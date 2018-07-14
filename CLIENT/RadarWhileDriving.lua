501323b17e33ca1c33f45432087a695df735fe6d
Flatracer <Flatracer@ymail.com>
2018-07-15 01:25:47 +0200

    Fixed link

--- CLIENT/RadarWhileDriving.lua
+++ CLIENT/RadarWhileDriving.lua
@@ -1,8 +1,8 @@
 -- Toggle this to enable the use of the big map (That doesn't work together with the LAMBDA ASI)
 local UseBigMap = true
--- Change this to change the big map toggle key while on foot (More Controls at https://wiki.fivem.net/wiki/Controls)
+-- Change this to change the big map toggle key while on foot (More Controls at http://docs.fivem.net/game-references/controls/)
 local BigMapKeyOnFoot = 44
--- Change this to change the big map toggle key while in a vehicle (More Controls at https://wiki.fivem.net/wiki/Controls)
+-- Change this to change the big map toggle key while in a vehicle (More Controls at http://docs.fivem.net/game-references/controls/)
 local BigMapKeyInVehicle = 131
 -- Change this to false to disable the big map in vehicles
 local BigMapInVehicles = true
